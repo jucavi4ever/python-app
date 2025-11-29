3164e6958cf93117dcc4f73c7878d63a5bd0b44a
Jucavi <jucavi4ever@gmail.com>
2025-11-29 14:07:26 -0400

    uses self-hsted runners

--- src/app.py
+++ src/app.py
@@ -14,7 +14,7 @@ def info():
     	'hostname': socket.gethostname(),
         'message': 'You are doing great, little human! <3',
         'deployed_on': 'kubernetes',
-        'test': 'change 3'
+        'test': 'change 43'
     })
 
 @app.route('/api/v1/healthz')
